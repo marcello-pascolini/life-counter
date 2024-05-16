cc1c9082fccc7aaed1ac82147cbbcd6e7475af82
marcello-pascolini <marcellopascolini967@gmail.com>
2024-05-16 10:28:28 +0200

    Clean code.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 import sassGlobImports from 'vite-plugin-sass-glob-import';
 
 
-
-
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),sassGlobImports()],
