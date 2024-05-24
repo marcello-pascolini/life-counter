0a326a45ff6f924d0d084248495c000e781f5591
marcello-pascolini <marcellopascolini967@gmail.com>
2024-05-24 12:51:43 +0200

    Displayed 4vs commander gird fix

--- src/components/VersusGrid/VersusGrid.tsx
+++ src/components/VersusGrid/VersusGrid.tsx
@@ -7,14 +7,21 @@ interface VersusGridInterface {
 const VersusGrid = ({selectedCards}:VersusGridInterface) => {
     console.log(selectedCards)
 
+    const gridColMap: any = {
+        0: { gridColumn: 1, gridRow: 1 },
+        1: { gridColumn: 2, gridRow: 1 },
+        2: { gridColumn: 1, gridRow: 2 },
+        3: { gridColumn: 2, gridRow: 2 },
+    }
+
     return (
         <div className="grid">
-
-        { selectedCards?.map((card: any) => (
-                    <div className={`col`}>
-                        <img src={card.image_uris.art_crop} alt={card.name} />
+            { 
+                selectedCards?.map((card: any, index: number) => (
+                    <div className={`col`} >
+                            <div className="rotated-bg" style={{  backgroundImage: `url(${card.image_uris.art_crop})` }}></div>
                     </div>
-        ))}
+            ))}
         </div>
     )
 }
